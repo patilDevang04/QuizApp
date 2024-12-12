b9066380f8fc04e221b6e464dc50862bcc71df86
patilDevang04 <patildevang04@gmail.com>
2024-12-12 17:39:59 +0530

    Checked can see both services live in Eureka dashboard.

--- quiz-service/src/main/java/com/devang/quiz_service/feignClient/QuestionClient.java
+++ quiz-service/src/main/java/com/devang/quiz_service/feignClient/QuestionClient.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 
-@FeignClient("QUESTION_SERVICE")
+@FeignClient("QUESTION-SERVICE")
 public interface QuestionClient {
     @GetMapping("question/generate")
     public ResponseEntity<List<Integer>> getQuestionsForQuiz
