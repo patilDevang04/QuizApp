0613f31ec10f8414705b07962f469c178b23286c
patilDevang04 <patildevang04@gmail.com>
2024-12-12 15:38:04 +0530

    Feign client for connection with Question service.

---
+++ quiz-service/src/main/java/com/devang/quiz_service/feignClient/QuestionClient.java
@@ -0,0 +1,25 @@
+package com.devang.quiz_service.feignClient;
+
+import com.devang.quiz_service.model.QuestionWrapper;
+import com.devang.quiz_service.model.Response;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.List;
+
+@FeignClient("QUESTION_SERVICE")
+public interface QuestionClient {
+    @GetMapping("question/generate")
+    public ResponseEntity<List<Integer>> getQuestionsForQuiz
+            (@RequestParam String categoryName, @RequestParam Integer numQuestions);
+
+    @PostMapping("question/getQuestions")
+    public ResponseEntity<List<QuestionWrapper>> getQuestionsFromId(@RequestBody List<Integer> questionIds);
+
+    @PostMapping("question/getScore")
+    public ResponseEntity<Integer> getScore(@RequestBody List<Response> responses);
+
+}
